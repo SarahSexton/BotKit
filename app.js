aad26a6391d6ba04e766cd17155eb06a2443ba82
Sarah <sarahjeannesexton@live.com>
2016-05-17 17:33:51 -0500

    Environment variables

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 var Botkit = require('botkit');
 var builder = require('botbuilder');
-
 var controller = Botkit.slackbot({ debug: false });
-
 var Wunderground = require('wundergroundnode');
-var myKey = '4d4516107fb8cea7';
-var wunderground = new Wunderground(myKey);
 
+var weatherKey = provess.env.WEATHER_KEY || "Missing weather API key"; 
+var wunderground = new Wunderground(weatherKey);
+
+var slackToken = provess.env.SLACK_TOKEN || "Missing Slack Token";
 
 var bot = controller.spawn({
-  token: 'xoxb-38628238758-ypaqFFvip3ObiT6GdqVukOQR',
+  token: provess.env.SLACK_TOKEN || "Missing Slack Token",
 }).startRTM()
 
-controller.hears('weather', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-  wunderground.conditions().request('60613', function (err, response) {
+controller.hears('weather', ['direct_message', 'direct_mention', 'mention'], function (bot, message) 
+{
+  wunderground.conditions().request('60613', function (err, response) 
+  {
     console.log(response);
-    bot.reply(message, 'The temperature is ' + response.current_observation.temp_f + ' degrees F.');
+    bot.reply(message, "It's *" + response.current_observation.weather + '* out right now, and'
+    + ' the temperature is ' + response.current_observation.temp_f + ' degrees F. ' + 
+    ' It feels like ' + response.current_observation.feelslike_f + ' F out there.');
+    //bot.reply(message, '');
   });
-
-controller.hears('thank', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-
+  controller.hears('thank', ['direct_message', 'direct_mention', 'mention'], function (bot, message) 
+  {
     bot.reply(message, 'I live to serve.');
+  });
+    controller.hears('love', ['direct_message', 'direct_mention', 'mention'], function (bot, message) 
+  {
+    bot.reply(message, 'You have not yet taught this robot how to love.');
+  });
 });
-});
-
-//var slackBot = new builder.SlackBot(controller, bot);
-//slackBot.add('/', function (session) {
-//  session.send('Hello World');
-//});
-
-//slackBot.listenForMentions();
